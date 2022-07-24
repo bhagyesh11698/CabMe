1c4d5b8bdae125eaad65e97f11a21ea3f4cbd510
Bhagyesh Patel <11698guru@gmail.com>
2022-07-24 16:14:45 -0400

    Added Material UI
    - Matrial UI
    - Client side website structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+
  const App = () =>{
     return (
-        <div>
-        <h1>Cab Me</h1>
-        </div>
+        <Container maxwidth ="lg">
+            <AppBar position="Static" color="inherit">
+                <Typography variant="h2" align = "center">Memories</Typography>
+                <img src = {memories} alt = "memories" height = "60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify= "space-between" alignItems = "stretch" spacing = {3}>
+                        <Grid item xs= {12} sm ={7}>
+                            //posts
+                        </Grid>
+                        <Grid item xs= {12} sm ={7}>
+                            //form
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
  }
 
